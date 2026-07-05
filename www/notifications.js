f050bfe78da9f441f5e72a4ecfeab2f0a018e9d2
* <jaziraarab15@gmail.com>
2026-07-05 16:56:17 +0700

    Tes permission notifikasi

--- www/notifications.js
+++ www/notifications.js
@@ -1,39 +1,10 @@
 async function initNotifications() {
 
+    alert("initNotifications dipanggil");
+
     const { LocalNotifications } = window.Capacitor.Plugins;
 
     const perm = await LocalNotifications.requestPermissions();
-    console.log("Permission:", perm);
-}
 
-async function schedulePrayerNotifications(t) {
-
-    const { LocalNotifications } = window.Capacitor.Plugins;
-
-    const prayers = [
-        { id: 1, name: "Subuh", time: t.Fajr },
-        { id: 2, name: "Zuhur", time: t.Dhuhr },
-        { id: 3, name: "Asar", time: t.Asr },
-        { id: 4, name: "Magrib", time: t.Maghrib },
-        { id: 5, name: "Isya", time: t.Isha }
-    ];
-
-    const notifications = prayers.map(p => {
-        const [hour, minute] = p.time.split(":").map(Number);
-
-        const date = new Date();
-        date.setHours(hour, minute, 0, 0);
-
-        return {
-            id: p.id,
-            title: "🕌 Waktu Salat",
-            body: `Sudah masuk waktu ${p.name}`,
-            schedule: { at: date },
-            sound: "default"
-        };
-    });
-
-    await LocalNotifications.schedule({
-        notifications
-    });
+    alert("Permission: " + JSON.stringify(perm));
 }
